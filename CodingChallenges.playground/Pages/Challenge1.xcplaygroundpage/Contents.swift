51bcb49c0b2946a518754f27ec70db0219ecb7b9
Spencer Schoeben <spencer@netspencer.com>
2020-01-10 22:50:41 -0800

    Adding intiial coding challenges playground

---
+++ CodingChallenges.playground/Pages/Challenge1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+func challenge1(input: String) -> Bool {
+    return true
+}
+
+assert(challenge1(input: "No duplicates") == true, "Challenge 1 failed")
+assert(challenge1(input: "abcdefghijklmnopqrstuvwxyz") == true, "Challenge 1 failed")
+assert(challenge1(input: "AaBbCc") == true, "Challenge 1 failed")
+assert(challenge1(input: "Hello, world") == false, "Challenge 1 failed")
